998e643f6611669dd7c933f0619000367cef8dcc
Alejandro Lopez <alejozepol@gmail.com>
2020-04-22 20:51:29 -0500

    [create] Module products

---
+++ src/app/products/products-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { ProductsComponent } from './components/products/products.component';
+import { ProductComponent } from './components/product/product.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: ProductsComponent,
+  },
+  {
+    path: '/product',
+    component: ProductComponent,
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class ProductsRoutingModule { }
