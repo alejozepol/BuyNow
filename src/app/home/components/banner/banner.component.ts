512826b4c7a8f2d5791146630d51e4bf74db2652
Alejandro Lopez <alejozepol@gmail.com>
2020-04-23 17:17:41 -0500

    Creacion de componente banner

---
+++ src/app/home/components/banner/banner.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import Swiper from 'swiper';
+
+@Component({
+  selector: 'app-banner',
+  templateUrl: './banner.component.html',
+  styleUrls: ['./banner.component.scss']
+})
+export class BannerComponent implements OnInit {
+
+  constructor() {
+  }
+  imagens: string[] = [
+    'assets/img/img1',
+    'assets/img/img2',
+    'assets/img/img2',
+  ];
+
+  ngOnInit(): void {
+    const mySwiper = new Swiper('.swiper-container', {
+      spaceBetween: 30,
+      centeredSlides: true,
+      autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
+  }
+
+}
