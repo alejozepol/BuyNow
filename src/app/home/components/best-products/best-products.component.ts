d1efc04d804a359f3d107780dc382fc1ddca26a9
Alejandro Lopez <alejozepol@gmail.com>
2020-05-02 17:02:30 -0500

    contenedor iterador de productos en el home

---
+++ src/app/home/components/best-products/best-products.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { ProductsService } from 'src/app/core/services/products.service';
+import { Product } from 'src/app/core/models/product.model';
+
+@Component({
+  selector: 'app-best-products',
+  templateUrl: './best-products.component.html',
+  styleUrls: ['./best-products.component.scss']
+})
+export class BestProductsComponent implements OnInit {
+
+  products: Product[] = [];
+  constructor(private productsService: ProductsService) { }
+
+  ngOnInit(): void {
+    this.getProductsService();
+  }
+
+  getProductsService() {
+    this.products = this.productsService.getAllproducts();
+  }
+
+}
