4a179a77223eb005e1280c80a7f44b303fb1cb73
Alejandro Lopez <alejozepol@gmail.com>
2020-05-26 23:39:35 -0500

    inicializando proyecto en pwa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 /* component */
 import { LayoutComponent } from './layout/layout.component';
 import { PageNotEntreringComponent } from './page-not-entrering/page-not-entrering.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 
 
@@ -33,6 +35,7 @@ import { PageNotEntreringComponent } from './page-not-entrering/page-not-entreri
     SharedModule,
     BrowserAnimationsModule,
     CoreModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
   ],
   providers: [],
   bootstrap: [AppComponent]
